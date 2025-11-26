a8a5062c819ab89fefc54408ff79a2d5b96b75af
Rahul Gupta <ray.bebarca@outlook.com>
2025-11-26 14:50:33 +0530

    Update Music_Store_Query.sql
    The solution to Q1 in Advanced section is corrected.

--- Music_Store_Query.sql
+++ Music_Store_Query.sql
@@ -115,26 +115,15 @@ Album, and Artist tables. Note, this one is tricky because the Total spent in th
 so you need to use the InvoiceLine table to find out how many of each product was purchased, and then multiply this by the price
 for each artist. */
 
-WITH best_selling_artist AS (
-	SELECT artist.artist_id AS artist_id, artist.name AS artist_name, SUM(invoice_line.unit_price*invoice_line.quantity) AS total_sales
-	FROM invoice_line
-	JOIN track ON track.track_id = invoice_line.track_id
-	JOIN album ON album.album_id = track.album_id
-	JOIN artist ON artist.artist_id = album.artist_id
-	GROUP BY 1
-	ORDER BY 3 DESC
-	LIMIT 1
-)
-SELECT c.customer_id, c.first_name, c.last_name, bsa.artist_name, SUM(il.unit_price*il.quantity) AS amount_spent
+SELECT c.customer_id, c.first_name, c.last_name, a.artist_name, SUM(il.unit_price*il.quantity) AS amount_spent
 FROM invoice i
 JOIN customer c ON c.customer_id = i.customer_id
 JOIN invoice_line il ON il.invoice_id = i.invoice_id
 JOIN track t ON t.track_id = il.track_id
 JOIN album alb ON alb.album_id = t.album_id
-JOIN best_selling_artist bsa ON bsa.artist_id = alb.artist_id
-GROUP BY 1,2,3,4
+JOIN artist a ON a.artist_id = alb.artist_id
+GROUP BY 1,4
 ORDER BY 5 DESC;
-
 
 /* Q2: We want to find out the most popular music Genre for each country. We determine the most popular genre as the genre 
 with the highest amount of purchases. Write a query that returns each country along with the top Genre. For countries where 
@@ -228,3 +217,4 @@ ORDER BY 1;
 /* source: www.youtube.com/@RishabhMishraOfficial */
 
 /* Thank You :) */
+
